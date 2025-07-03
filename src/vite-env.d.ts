5bd9672ab9b9a2864aac29f4f1d0c50e02fcb9ce
permalik <permalik@protonmail.com>
2025-07-02 22:59:55 -0500

    feat: add primevue, eslint, config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+declare module "*.vue" {
+    import type {DefineComponent} from "vue";
+    const component: DefineComponent<object, object, never>;
+    export default component;
+}
